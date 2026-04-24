97c71f8566319df556dbfd68273b2095aece7247
usmanzafarofficial <usmanzafarofficial125@gmail.com>
2026-04-24 21:34:21 +0500

    fixed

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,4 +1,4 @@
 /// <reference types="vite/client" />
 
-declare module 'gsap-trial/SplitText';
-declare module 'gsap-trial/ScrollSmoother';
+declare module 'gsap/SplitText';
+declare module 'gsap/ScrollSmoother';
